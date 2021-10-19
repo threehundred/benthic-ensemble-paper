7b2c0cadae3145847810cb53055b8493aa896cfa
threehundred <2068244+threehundred@users.noreply.github.com>
2021-10-19 20:27:35 +0800

    Adding get data script and weights

---
+++ get_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir data
+wget "https://research-repository.uwa.edu.au/files/147877007/RowleyShoalsBenthicImagePatches.zip" -O ./data/RowleyShoalsBenthicImagePatches.zip
+cd ./data
+unzip ./RowleyShoalsBenthicImagePatches.zip
